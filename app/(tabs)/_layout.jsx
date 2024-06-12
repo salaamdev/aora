701feaaef61bbecc8f8ebe30554260e5f60b0222
salaam <abdisalamxhassan@gmail.com>
2024-06-12 18:15:31 +0300

    clean tabs layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -4,7 +4,7 @@ import {icons} from '../../constants'
 
 const TabIcon = ({icon, color, name, focused}) => {
     return (
-        <View>
+        <View className="items-center justify-center gap-2">
             <Image
                 source={icon}
                 resizeMode="contain"
@@ -36,6 +36,51 @@ const TabsLayout = () => {
                               icon={icons.home}
                               color={color}
                               name="Home"
+                              focused={focused}
+                          />         
+                      )
+                  }}
+              />
+              <Tabs.Screen
+                  name="bookmark"
+                  options={{
+                      title: 'Bookmark',
+                      headerShown: false,
+                      tabBarIcon: ({color, focused}) => (
+                          <TabIcon
+                              icon={icons.bookmark}
+                              color={color}
+                              name="Bookmark"
+                              focused={focused}
+                          />         
+                      )
+                  }}
+              />
+              <Tabs.Screen
+                  name="create"
+                  options={{
+                      title: 'Create',
+                      headerShown: false,
+                      tabBarIcon: ({color, focused}) => (
+                          <TabIcon
+                              icon={icons.plus}
+                              color={color}
+                              name="Create"
+                              focused={focused}
+                          />         
+                      )
+                  }}
+              />
+              <Tabs.Screen
+                  name="profile"
+                  options={{
+                      title: 'Profile',
+                      headerShown: false,
+                      tabBarIcon: ({color, focused}) => (
+                          <TabIcon
+                              icon={icons.profile}
+                              color={color}
+                              name="Profile"
                               focused={focused}
                           />         
                       )
