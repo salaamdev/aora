6aa94e911b8060d72408173c90863861c434953d
salaam <abdisalamxhassan@gmail.com>
2024-06-12 18:10:34 +0300

    home screen added

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import {Tabs, Redirect} from 'expo-router'
+import {icons} from '../../constants'
+
+const TabIcon = ({icon, color, name, focused}) => {
+    return (
+        <View>
+            <Image
+                source={icon}
+                resizeMode="contain"
+                tintColor={color}
+                className="w-6 h-6"
+            />
+            <Text className={`${focused? 'font-psemibold':'font-pregular'} text-xs`}>
+                {name}
+            </Text>
+        </View>
+    )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+      <>
+          <Tabs
+              screenOptions={{
+                  tabBarShowLabel: false
+              }}
+          >
+              <Tabs.Screen
+                  name="home"
+                  options={{
+                      title: 'Home',
+                      headerShown: false,
+                      tabBarIcon: ({color, focused}) => (
+                          <TabIcon
+                              icon={icons.home}
+                              color={color}
+                              name="Home"
+                              focused={focused}
+                          />         
+                      )
+                  }}
+              />
+          </Tabs>
+      </>
   )
 }
 
