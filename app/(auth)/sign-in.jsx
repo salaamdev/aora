190c7d45e3e0c79654ce5de947f3544f2be786a8
salaam <abdisalamxhassan@gmail.com>
2024-06-15 16:03:01 +0300

    sign in page initial commit

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import {SafeAreaView} from 'react-native-safe-area-context'
+import {images} from '../../constants'
+import FormField from '../../components/FormField'
+
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView className=''>
+        <View className='w-full justify-center h-full px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]'
+          />
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Log in to Aora</Text>
+          <FormField
+            
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
